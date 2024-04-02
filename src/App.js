dea949f1a65ab70705d19cfc95336f2d7640b873
Dharmnath Shinde <Dharmnath.Shinde@prihirtech.com>
2024-04-02 17:14:52 +0530

    api fetching and routing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
-import React from 'react';
-import ConditionRender from './ConditionRender';
-import Form3 from './Form3';
-import Carousl from './Carousl';
-import Promise from './Promise';
-import Routers from './Routers';
-import About from './About';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Contact from './Contact';
-import Api, { Info } from './Api';
+import React, { useEffect, useState } from "react";
+import Users from "./User";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserDetails from "./UserDetails";
 
-function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Routers />
-    },
-    {
-      path: '/about',
-      element: <><Routers /><About /></>
-    },
-    {
-      path: '/contact',
-      element: <><Routers /><Contact /></>
-    },
-    {
-      loader:Info,
-      path: '/api',
-      element: <Api />
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [activeUser, setActiveUser] = useState({});
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+     .then((response) => response.json())
+     .then((json) => {
+        setUsers(json);
       
-    }
-  ]);
-  
+      });
+  }, []);
+
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <div
+      style={{
+        
+        background: "#2F363F",
+        color: "#53E0BC",
+        width: "100%",
+        height: "100vh",
+        padding: "16px",
+      }}
+    >
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+                <Users users={users} activeUser={activeUser} setActiveUser={setActiveUser} />
+      
+            }
+          />
+          <Route
+            path="/:Id"
+            element={
+                <UserDetails    />
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
   );
 }
-
-export default App;
